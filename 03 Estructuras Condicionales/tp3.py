a3c8886a0eefa8fce13e51dc76fb1dcbf52b4bc3
Lorena Tropeano <ltropeano81@gmail.com>
2025-04-13 21:45:34 -0300

    guradando ejercicios del 1 al 5

---
+++ 03 Estructuras Condicionales/tp3.py
@@ -0,0 +1,58 @@
+#1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años,
+#deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”.
+
+edad= int(input("Por favor ingrese su edad : "))
+if edad > 18:
+    print("Es mayor de edad")
+
+#2) Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, deberá
+#mostrar por pantalla un mensaje que diga “Aprobado”; en caso contrario deberá mostrar el
+#mensaje “Desaprobado”.
+
+nota= int(input("Por favor ingrese su nota: "))
+if nota >=6:
+        print("Aprobado")
+else:
+        print("Desaprobado")
+
+#3) Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un
+#número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso
+#contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del
+#operador de módulo (%) en Python para evaluar si un número es par o impar
+
+numero= int(input("Ingrsa un numero par"))
+if numero % 2==0:
+       print ("Ha ingresado un numero par")
+else:
+       print("Por favor, ingrese un numero par")
+
+#4) Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las
+#siguientes categorías pertenece:
+#● Niño/a: menor de 12 años.
+#● Adolescente: mayor o igual que 12 años y menor que 18 años.
+#● Adulto/a joven: mayor o igual que 18 años y menor que 30 años.
+#● Adulto/a: mayor o igual que 30 años.
+
+años= int(input("ingrese su edad:"))
+if años < 12 :
+       print ("Eres un niño/a")
+if 12 >= años < 18:
+       print ("Eres un adolescente") 
+if 18 <= años < 30:
+       print("Eres un adulto/a joven")
+else:
+       print("Eres un adulto/a")
+
+#5) Escribir un programa que permita introducir contraseñas de entre 8 y 14 caracteres
+#(incluyendo 8 y 14). Si el usuario ingresa una contraseña de longitud adecuada, imprimir por en
+#pantalla el mensaje "Ha ingresado una contraseña correcta"; en caso contrario, imprimir por
+#pantalla "Por favor, ingrese una contraseña de entre 8 y 14 caracteres". Nota: investigue el uso
+#de la función len() en Python para evaluar la cantidad de elementos que tiene un iterable tal
+#como una lista o un string.
+contraseña= (input("Ingrese una contraseña entre 8 y 14 caracteres"))
+if 8 >= len(contraseña)<=10:
+    print("Ha ingresado una contraseña correcta")
+else:
+    print("Por favor, ingrese una contraseña entre 8 y 14 caracteres")
+
+       
