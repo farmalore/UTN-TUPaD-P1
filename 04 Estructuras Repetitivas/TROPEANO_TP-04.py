e80ee567c667ee4d43e2a12d20da5739ae5bdb64
Lorena Tropeano <ltropeano81@gmail.com>
2025-04-17 08:45:34 -0300

    trabajo terminado

--- 04 Estructuras Repetitivas/TROPEANO_TP-04.py
+++ 04 Estructuras Repetitivas/TROPEANO_TP-04.py
@@ -67,6 +67,51 @@ print(f"La suma entre 0 y el {num_natural} es {sum_num}")
 #negativos y cuántos son positivos. (Nota: para probar el programa puedes usar una cantidad 
 #menor, pero debe estar preparado para procesar 100 números con un solo cambio).
 
+cantidad= 100
+num_pares=0
+num_impares=0
+num_positivos=0
+num_negativos=0
+for n in range(cantidad):
+    num_entero=int(input(f"Ingrese el numero entero {n + 1}: "))
+    if num_entero%2==0:
+        num_pares +=1
+    else:
+        num_impares +=1
+    if num_entero > 0:
+        num_positivos +=1
+    else:
+        num_negativos +=1
+print(f"Ingresaste {num_pares} numeros pares, {num_impares} numeros impares, {num_positivos} numeros positivos y {num_negativos} numeros negativos")
+
+#9) Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la 
+#media de esos valores. (Nota: puedes probar el programa con una cantidad menor, pero debe 
+#poder procesar 100 números cambiando solo un valor).
+cant=100
+total=0
+for n in range(cant):
+    num_ingresado=int(input(f"Ingrese el numero entero {n + 1}: "))
+    total += num_ingresado
+promedio= total/cant
+print(f"El promedio de los numeros ingresados es {promedio}")
+
+#10) Escribe un programa que invierta el orden de los dígitos de un número ingresado por el 
+#usuario. Ejemplo: si el usuario ingresa 547, el programa debe mostrar 745.
+
+n=int(input("Ingrese un numero de dos o mas digitos: "))
+num_invertido=0
+while n >0:
+    remanente= n%10
+    num_invertido=(num_invertido * 10) + remanente
+    n= n//10
+print(f" {n} invertido es {num_invertido}")
+
+
+
+
+
+
+
 
 
 
