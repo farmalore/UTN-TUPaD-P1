e6d26f715227caf11f5e5b3f30c382230eab1c9e
Lorena Tropeano <ltropeano81@gmail.com>
2025-04-23 09:55:46 -0300

    terminado

--- 06 Datos complejos/TROPEANO_TP-06.py
+++ 06 Datos complejos/TROPEANO_TP-06.py
@@ -37,6 +37,52 @@ numeros.remove(max(numeros))
 print(numeros)
 #en una lista dad utilizando la funcion remove + max = elimina el valor mas grande de la lista.
 
+#6) Crear una lista con números del 10 al 30 (incluído), haciendo saltos de 5 en 5 y mostrar por
+#pantalla los dos primeros.
+lista_5=list(range(10,31,5))
+print(lista_5[0:2])
+
+#7) Reemplazar los dos valores centrales (índices 1 y 2) de la lista “autos” por dos nuevos valores
+#cualesquiera.
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1]= "yaris"
+autos[2]= "etios"
+print(autos)
+
+#8) Crear una lista vacía llamada "dobles" y agregar el doble de 5, 10 y 15 usando append
+#directamente. Imprimir la lista resultante por pantalla.
+dobles=[]
+dobles.append(2*5)
+dobles.append(2*10)
+dobles.append(2*15)
+print(dobles)
+
+#9) Dada la lista “compras”, cuyos elementos representan los productos comprados por
+#diferentes clientes:
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],
+["agua"]]
+#a) Agregar "jugo" a la lista del tercer cliente usando append.
+#b) Reemplazar "fideos" por "tallarines" en la lista del segundo cliente.
+#c) Eliminar "pan" de la lista del primer cliente.
+#d) Imprimir la lista resultante por pantalla
+compras[2].append("jugo")
+compras[1][1]="tallarines"
+compras[0].remove("pan")
+print(compras)
+
+#10) Elaborar una lista anidada llamada “lista_anidada” que contenga los siguientes elementos:
+#● Posición lista_anidada[0]: 15
+#● Posición lista_anidada[1]: True
+#● Posición lista_anidada[2][0]: 25.5
+#● Posición lista_anidada[2][1]: 57.9
+#● Posición lista_anidada[2][2]: 30.6
+#● Posición lista_anidada[3]: False
+#Imprimir la lista resultante por pantalla
+
+lista_anidada=[15],["True"],[25.5,57.9,30.6],["False"]
+print(lista_anidada)
+
+
 
 
 
